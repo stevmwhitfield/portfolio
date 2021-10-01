340231f43f91cbf7763874220169d95ded3f22bf
Whitfield <swhitfield7764@floridapoly.edu>
2021-10-01 09:39:11 -0400

    Responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,34 @@
 import React from "react";
 
+import * as styles from "../styles/Footer.module.scss";
+
 const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer id={styles.footer}>
+      <div id={styles.about}>
         <h4>About Me</h4>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
           doloremque consectetur alias, tempore similique a cum perferendis
           mollitia natus voluptas assumenda voluptatibus in saepe.
         </p>
-        <p>Copyright &copy; 2021. All Rights Reserved.</p>
       </div>
-      <div>
+      <div id={styles.social}>
         <a href="github.com" target="_blank">
-          GitHub
+          <i class="fab fa-github"></i>
+          {/* GitHub */}
         </a>
         <a href="linkedin.com" target="_blank">
-          LinkedIn
+        <i class="fab fa-linkedin"></i>
+          {/* LinkedIn */}
         </a>
         <a href="twitter.com" target="_blank">
-          Twitter
+        <i class="fab fa-twitter"></i>
+          {/* Twitter */}
         </a>
+      </div>
+      <div id={styles.copyright}>
+        <p>Copyright &copy; 2021. All Rights Reserved.</p>
       </div>
     </footer>
   );
