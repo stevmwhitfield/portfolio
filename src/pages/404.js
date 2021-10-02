90b5c47de2660f2248af92687712ab4e9e9d7e31
BuildTools <unconfigured@null.spigotmc.org>
2021-10-02 13:00:40 -0400

    Create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Page Not Found | Steven Whitfield</title>
+        <meta
+          name="description"
+          content="A collection of my works and projects."
+        />
+        <link rel="canonical" href="https://stevenwhitfield.netlify.app/work" />
+      </Helmet>
+      <h1>Error 404</h1>
+      <h2>Page not found.</h2>
+      <Link to="/">Return to home.</Link>
+    </Layout>
+  );
+};
+
+export default PageNotFound;
