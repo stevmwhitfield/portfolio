8372f2666839c2b9f29e0a70592b64d6c8b92f0a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-02 15:11:42 -0400

    Edit form properties

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,18 @@ import * as styles from "../styles/Form.module.scss";
 
 const Form = () => {
   return (
-    <form id={styles.form} name="contact" netlify>
+    <form id={styles.form} name="contact" method="POST" netlify>
       <div id={styles.nameContainer} className={styles.formField}>
         <label>Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" required minLength="2" />
       </div>
       <div id={styles.emailContainer} className={styles.formField}>
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" required />
       </div>
       <div id={styles.messageContainer} className={styles.formField}>
         <label>Message</label>
-        <textarea name="message" cols="30" rows="8"></textarea>
+        <textarea name="message" cols="30" rows="8" required></textarea>
       </div>
       <button type="submit">Send</button>
     </form>
