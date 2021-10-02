406b0382f84a343689cf7896eebf26a9613fede4
BuildTools <unconfigured@null.spigotmc.org>
2021-10-02 13:08:20 -0400

    Changed info in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,11 +10,7 @@ const PageNotFound = () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Page Not Found | Steven Whitfield</title>
-        <meta
-          name="description"
-          content="A collection of my works and projects."
-        />
-        <link rel="canonical" href="https://stevenwhitfield.netlify.app/work" />
+        <meta name="description" content="Error 404: Page not found." />
       </Helmet>
       <h1>Error 404</h1>
       <h2>Page not found.</h2>
