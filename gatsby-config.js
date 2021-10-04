2aea86d92a2f52e6983b5f8154b50d9a4613b153
Steven Whitfield <stevmwhitfield@gmail.com>
2021-10-04 18:39:35 -0400

    Remove unnecessary query

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,34 +30,16 @@ module.exports = {
               path
             }
           }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
-              }
-            }
-          }
         }
       `,
         resolveSiteUrl: () => siteUrl,
         resolvePages: ({
           allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
         }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node
-            acc[uri] = node
-
-            return acc
-          }, {})
+          
 
           return allPages.map(page => {
-            return { ...page, ...wpNodeMap[page.path] }
+            return { ...page }
           })
         },
         serialize: ({ path, modifiedGmt }) => {
