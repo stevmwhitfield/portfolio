44cdb676fb45988ce70b7b298bdcc36715b161b8
BuildTools <unconfigured@null.spigotmc.org>
2021-10-05 11:59:36 -0400

    Change sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,38 +45,13 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        query: `
-        {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-        }
-      `,
-        resolveSiteUrl: () => siteUrl,
-        resolvePages: ({ allSitePage: { nodes: allPages } }) => {
-          return allPages.map(page => {
-            return { ...page };
-          });
-        },
-        serialize: ({ path, modifiedGmt }) => {
-          return {
-            url: path,
-            lastmod: modifiedGmt,
-          };
-        },
-      },
-    },
-    {
       resolve: `gatsby-plugin-sass`,
       options: {
         sassRuleTest: /\.global\.s(a|c)ss$/,
         sassRuleModulesTest: /\.module\.s(a|c)ss$/,
       },
     },
+    "gatsby-plugin-advanced-sitemap",
     "gatsby-plugin-smoothscroll",
     "gatsby-plugin-react-helmet",
   ],
